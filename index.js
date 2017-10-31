26ce7f75b659985abc11bba0a537132e953ec6f7
Mark Moissette <kaosat.dev@gmail.com>
2017-10-31 16:43:43 +0100

    feat(foo): added some other feature (#1)
    * feat(foo): added some other feature
    * chore(pkg): removed node 6 testing for faster tests

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const bar = 'yipee'
 const nope = () => bar
 const fooyeah = () => console.log('fooyeah')
 const foobar = () => console.log('foobar')
+const blurk = () => console.log('blurk')
 
-module.exports = {nope, fooyeah, foobar}
+module.exports = {nope, fooyeah, foobar, blurk}
